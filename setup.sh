90757fe42dee85171a896a3c6fc2eade0fc10087
f-yusei <fkys2932@gmail.com>
2024-06-17 20:40:27 +0900

    fix: envファイルが生成されるように

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+cp .env.example .env
+
 docker-compose up -d --build
 
 echo 'Waiting for kadai-app to be ready...';
-sleep 60
+sleep 10
 
 
-docker-compose exec php sh -c 'composer dump-autoload'
+docker-compose exec app sh -c 'composer install'
+docker-compose exec app sh -c 'composer dump-autoload'
 docker-compose exec app sh -c 'php artisan migrate --seed'
